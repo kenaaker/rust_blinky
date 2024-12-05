bcd90945cd09d1467fcbc91f4ddb2ffe16e382eb
Ken Aaker <kenaaker@gmail.com>
2024-12-05 10:06:15 -0600

    Update blinky.rs

--- src/blinky.rs
+++ src/blinky.rs
@@ -1,1 +1,36 @@
+//! Blinks an LED
+//!
+//! The following wiring is assumed:
+//! - LED => LED_BUILTIN
+//! https://github.com/espressif/arduino-esp32/blob/d47771f2cc649c3cd52a3f6eb3d9b97c82005ffb/variants/XIAO_ESP32C6/pins_arduino.h#L13
 
+
+#![no_std]
+#![no_main]
+
+
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Io, Level, Output},
+    prelude::*,
+};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    // Set GPIO0 as an output, and set its state high initially.
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = Output::new(LED_BUILTIN, Level::High);
+
+    let delay = Delay::new();
+
+    loop {
+        led.toggle();
+        delay.delay_millis(250);
+        led.toggle();
+        // or using `fugit` duration
+        delay.delay_millis(250);
+    }
+}
